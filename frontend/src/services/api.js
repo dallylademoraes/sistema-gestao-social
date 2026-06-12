b66eb10efe27498ca8928e2af6bef050c3304aa6
Dallyla <moraesdallyla6@gmail.com>
2026-06-12 13:59:20 -0300

    return mundanca

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: import.meta.env.VITE_API_URL + '/api' })
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL + '/api', withCredentials: true })
 
 // Interceptor de Requisição: Adiciona o Token de forma segura
 api.interceptors.request.use((config) => {
